2bdac670cfe7f0603cd7aa7fc387a236b0d3c7e3
hchittanuru3 <hemanthc98@outlook.com>
2021-02-28 13:43:07 -0600

    V1 of Dataframe code

--- rarecrowds/rarecrowds.py
+++ rarecrowds/rarecrowds.py
@@ -1,7 +1,9 @@
 import json
 import os
+from typing import Dict, List
 
-from google.protobuf.json_format import Parse
+from google.protobuf.json_format import Parse, MessageToJson
+import pandas as pd
 
 from rarecrowds.phenopackets_pb2 import Phenopacket
 
@@ -9,6 +11,7 @@ from rarecrowds.phenopackets_pb2 import Phenopacket
 class PhenotypicDatabase:
     def __init__(self):
         self.db = {}
+        self.fields = ["id", "phenotypicFeatures", "genes", "diseases", "subject.id"]
 
     def add_phenopacket(self, phenopacket: Phenopacket):
         self.db[phenopacket.id] = phenopacket
@@ -20,3 +23,16 @@ class PhenotypicDatabase:
     def load_from_folder(self, folder_path: str):
         for file in os.listdir(folder_path):
             self.add_phenopacket(self.load_from_file(folder_path + "/" + file))
+
+    def generate_list_of_dicts(self) -> List[Dict]:
+        return_list = []
+        for k, v in self.db.items():
+            return_list.append(json.loads(MessageToJson(v)))
+        return return_list
+
+    def generate_dataframe(self) -> pd.core.frame.DataFrame:
+        json_list = []
+        for k, v in self.db.items():
+            json_list.append(json.loads(MessageToJson(v)))
+        df = pd.json_normalize(json_list)
+        return df[self.fields]
